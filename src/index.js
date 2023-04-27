8fbe0b57d215496e4280721391655149f093ae57
Алина Радюкевич <alina.makaroshka@outlook.com>
2023-04-28 00:43:05 +0300

    feat: add setStartHiddenKeys function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import initKeyboard from "./scripts/start.js";
+import setStartHiddenKeys from "./scripts/hidden-keys.js";
 
 initKeyboard();
+
+let language = "";
+
+if (localStorage.getItem("currLanguage") === null || localStorage.getItem("currLanguage") === "en") {
+  language = "en";
+  setStartHiddenKeys(language);
+} else {
+  language = "ru";
+  setStartHiddenKeys(language);
+}
